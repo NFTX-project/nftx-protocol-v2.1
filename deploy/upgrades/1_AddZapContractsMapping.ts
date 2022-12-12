1414bd2c34aa03df51ff57a75f1f306009977485
apoorvlathey <latheyapoorv@gmail.com>
2022-12-13 05:28:20 +0530

    fix upgrades script

--- deploy/upgrades/1_AddZapContractsMapping.ts
+++ deploy/upgrades/1_AddZapContractsMapping.ts
@@ -11,25 +11,50 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const config = deployConfig[network.name];
 
   // @note upgrading will fail here as proxies owned by MultiProxyController
-  // so let the following code deploy new implementation (despite error), store it in deployments folder and manually upgrade proxy implementation
+  // so let the following code deploy new implementation (despite error), store it in deployments folder and then upgrade proxy implementation
 
-  const NFTXVaultFactoryUpgradeable = await deploy(
-    "NFTXVaultFactoryUpgradeable",
-    {
+  try {
+    const NFTXVaultFactoryUpgradeable = await deploy(
+      "NFTXVaultFactoryUpgradeable",
+      {
+        from: deployer,
+        proxy: {
+          proxyContract: "OpenZeppelinTransparentProxy",
+        },
+        log: true,
+      }
+    );
+  } catch (e) {}
+
+  try {
+    const NFTXInventoryStaking = await deploy("NFTXInventoryStaking", {
       from: deployer,
       proxy: {
         proxyContract: "OpenZeppelinTransparentProxy",
       },
       log: true,
-    }
+    });
+  } catch (e) {}
+
+  // upgrade with new implementation
+  const NFTXVaultFactoryUpgradeable = await deployments.get(
+    "NFTXVaultFactoryUpgradeable"
   );
-  const NFTXInventoryStaking = await deploy("NFTXInventoryStaking", {
-    from: deployer,
-    proxy: {
-      proxyContract: "OpenZeppelinTransparentProxy",
-    },
-    log: true,
-  });
+  const NFTXInventoryStaking = await deployments.get("NFTXInventoryStaking");
+  await execute(
+    "MultiProxyController",
+    { from: deployer },
+    "upgradeProxyTo",
+    0,
+    NFTXVaultFactoryUpgradeable.implementation
+  );
+  await execute(
+    "MultiProxyController",
+    { from: deployer },
+    "upgradeProxyTo",
+    5,
+    NFTXInventoryStaking.implementation
+  );
 
   // add zaps to `zapContracts` mapping
   const NFTXStakingZap = await deployments.get("NFTXStakingZap");
