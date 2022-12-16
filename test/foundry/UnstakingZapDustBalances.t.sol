8208e82ba327ba30931d5e20814d843144fd7cc3
Tom Wade <twade7@gmail.com>
2022-12-16 23:34:45 +0000

    initial commit

---
+++ test/foundry/UnstakingZapDustBalances.t.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import 'forge-std/Test.sol';
+
+// import '../../../contracts/solidity/NFTXUnstakingInventoryZap.sol';
+
+
+/**
+ * We are seeing users that are trying to unstaking inventory (unstaking to NFTs) are
+ * getting execution reverted under certain circumstances.
+ * 
+ * So far it seems like the issue occurs when...
+ * 
+ * - The amount of vTokens in the underlying xToken staked amount is 1wei short... this
+ *   has been seen with 0.99999999999 tokens and with 7.9999999999 tokens.
+ * - There has been no activity on the vault since the user staked their position (i.e.
+ *   the remain 1wei short, any fee generating activity will provide them enough tokens
+ *   to exit with all their NFTs plus dust token amounts in change)
+ * - There is an amount of the vToken sitting in the Unstaking Inventory Zap
+ * 
+ * https://app.shortcut.com/nftx/story/719/unstaking-zap-execution-reverted-with-dust-balances
+ * https://www.notion.so/nftx/Execution-Reverted-on-Inventory-Unstaking-ca871d36d1894e8ba6af98967e630bea
+ */
+contract UnstakingZapDustBalances is Test {
+
+    /// Store our mainnet fork information
+    // uint mainnetFork;
+    // uint internal constant BLOCK_NUMBER = 16_189_167;
+
+    // address internal constant AFFECTED_USER = 0x82cA345C167BDCAd56FbedC7cc85a488885BDAdc;
+    // uint internal constant VAULT_ID = 1;
+    // address internal constant VAULT_TOKEN = 0xfC0247CdAAbC166423915077b666FA3bB9d1ee4d;
+    // address payable internal constant UNSTAKING_ZAP = payable(0x51d660Ba5c218b2Cf33FBAcA5e3aBb8aEff3543B);
+
+    function setUp() public {
+        /*
+        // Generate a mainnet fork
+        mainnetFork = vm.createFork(vm.envString('MAINNET_RPC_URL'));
+
+        // Select our fork for the VM
+        vm.selectFork(mainnetFork);
+
+        // Set our block ID to a specific, test-suitable number
+        vm.rollFork(BLOCK_NUMBER);
+
+        // Confirm that our block number has set successfully
+        assertEq(block.number, BLOCK_NUMBER);
+        */
+    }
+
+    function test_CanTriggerIssue() public {
+        /*
+        // Connect to our affect user to mock future calls as their wallet
+        vm.startPrank(AFFECTED_USER);
+
+        // Find the number of available NFTs based on the held xToken, as well as if we
+        // are `shortByTinyAmount`, which (I believe) flags the 1 wei issue
+        (uint numNfts, bool shortByTinyAmount) = NFTXUnstakingInventoryZap(UNSTAKING_ZAP).maxNftsUsingXToken(
+            VAULT_ID,
+            AFFECTED_USER,
+            VAULT_TOKEN
+        );
+
+        assertEq(numNfts, 2);
+        assertEq(shortByTinyAmount, true);
+
+        // Attempt to unstake our inventory
+        NFTXUnstakingInventoryZap(UNSTAKING_ZAP).unstakeInventory(
+            VAULT_ID,
+            numNfts,
+            0  // remainingPortionToUnstake
+        );
+
+        vm.stopPrank();
+        */
+    } 
+
+}
